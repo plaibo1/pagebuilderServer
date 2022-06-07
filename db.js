a3eb9fa7591d572ef70a6c2caf6f947c31129da6
plajboi <lyskov.2000@mai.ru>
2022-06-07 08:10:43 +0300

    heroku var

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-// postgres://YourUserName:YourPassword@localhost:5432/YourDatabase
-
 // module.exports = new Sequelize(
 //     process.env.DB_NAME,
 //     process.env.DB_USER,
