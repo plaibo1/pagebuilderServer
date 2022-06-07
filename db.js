e2e2a3646526c25f2eef746224f9be29e31d8e7f
plajboi <lyskov.2000@mai.ru>
2022-06-07 07:44:11 +0300

    rewrite db connection

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
 const { Sequelize } = require('sequelize');
 
+// postgres://YourUserName:YourPassword@localhost:5432/YourDatabase
+
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT
+//     }
+// )
+
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DATABASE_URL,
     {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        dialect: 'postgres'
     }
 )
